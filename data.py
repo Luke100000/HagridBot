144d38af1d7bd6b12e37bbc9847659b076bdbf60
Luke100000 <jonas.peche@aon.at>
2023-07-21 22:44:40 +0200

    refactor

---
+++ data.py
@@ -0,0 +1,12 @@
+HAGRID_BEDROCK = """
+Oh, blimey! I can't believe me ears, I really can't! What's the world come to when people start requestin' Minecraft Bedrock support for Minecraft Java, eh? It's like mixin' potions that should never be mixed! It's like tryin' to put a hippogriff in a teacup!
+
+Minecraft Java and Bedrock are like two different magical creatures, they are! They've got their own spells, their own enchantments, and their own way of doin' things. It's like tryin' to teach a Hungarian Horntail ballet – it just ain't natural!
+
+Minecraft Java is the original, the OG version, with all its glorious mods and customizability. It's like a finely brewed potion with all the ingredients carefully measured. But now, folks want to mix it with Bedrock, which is like tryin' to put a blast-ended skrewt in a tutu! It'll be messy, it will!
+
+And don't even get me started on the technical side of things! It's like tryin' to tame a blast-ended skrewt – it's a wild mess! The codes, the compatibility issues, it's like puttin' a Niffler in a treasure room and expectin' it not to cause chaos!
+
+I'm all for a bit of magical experimentation, but some things just shouldn't be tampered with, and mixin' Java and Bedrock is one of 'em! It's like tryin' to teach a Thestral how to dance the tango – it's just not meant to be!
+
+So, let's keep things as they are, shall we? Let Java be Java, and Bedrock be Bedrock, and let's not go mixin' potions that could explode in our faces! Stick to the magic you know, and let's enjoy each version for what it is – a magical adventure in its own right!"""
